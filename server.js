d5ada087e1d9994fa26dea1d0b4f1a13abbe1d0d
Tim Machnacki <machnackit@gmail.com>
2023-01-20 15:11:36 -0500

    production build

--- server.js
+++ server.js
@@ -54,7 +54,11 @@ app.use("/posts", postRoutes);
 
 /* PRODUCTION */
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 /* MONGOOSE SETUP */
